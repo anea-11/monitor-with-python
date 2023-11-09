9dadacfe4ac943cbfafd1c5cd6c5157fff2311cb
andrijana <andrijana.ojdanic@tutanota.com>
2023-11-09 04:55:02 +0100

    Notify when site is unreachable

--- monitor-website.py
+++ monitor-website.py
@@ -7,15 +7,24 @@ JENKINS_URL = 'http://18.197.142.203:8080'
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
-response = requests.get(JENKINS_URL)
-if response.status_code == 200:
-    print("App is healthy")
-else:
-    print('App is not healthy')
-
+def send_email_notification(email_msg):
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.starttls()
         smtp.ehlo()
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        msg = "Subject: SITE DOWN!\nFix the issue!"
+        msg = f"Subject: SITE DOWN!\n{email_msg}"
         smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
+
+try:
+    response = requests.get(JENKINS_URL)
+    if response.status_code == 200:
+        print("App is healthy")
+    else:
+        print(f'App is not healthy! Status code: {response.status_code}')
+        msg = f"Subject: SITE DOWN!\n App returned status code {response.status_code}"
+        send_email_notification(msg)
+
+except Exception as ex:
+    print(f'Connection error happened')
+    msg = "Subject: SITE DOWN!\n App not accessible"
+    send_email_notification(msg)
