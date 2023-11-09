9ed07fd0f0f1d239a8a6f4b51646fa640091f524
andrijana <andrijana.ojdanic@tutanota.com>
2023-11-09 04:45:01 +0100

    Configure email notification when app is not healthy

--- monitor-website.py
+++ monitor-website.py
@@ -1,8 +1,21 @@
 import requests
+import smtplib
+import os
 
-response = requests.get('my-url')
+JENKINS_URL = 'http://18.197.142.203:8080'
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+response = requests.get(JENKINS_URL)
 if response.status_code == 200:
     print("App is healthy")
 else:
-    print("App is not healthy")
+    print('App is not healthy')
 
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        msg = "Subject: SITE DOWN!\nFix the issue!"
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
